35c901df9b9755e3d2271c958d9f67857b8c6393
pquindec <xndre.qc@gmail.com>
2020-01-16 10:14:37 -0500

    add login

---
+++ listacliente/Form4.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form4
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtusuario = New System.Windows.Forms.TextBox()
+        Me.txtpass = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(54, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(177, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Inicio de sesion"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(76, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Usuario"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(62, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Contraseña"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(113, 174)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Aceptar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtusuario
+        '
+        Me.txtusuario.Location = New System.Drawing.Point(125, 77)
+        Me.txtusuario.Name = "txtusuario"
+        Me.txtusuario.Size = New System.Drawing.Size(100, 20)
+        Me.txtusuario.TabIndex = 4
+        '
+        'txtpass
+        '
+        Me.txtpass.Location = New System.Drawing.Point(125, 117)
+        Me.txtpass.Name = "txtpass"
+        Me.txtpass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtpass.Size = New System.Drawing.Size(100, 20)
+        Me.txtpass.TabIndex = 5
+        '
+        'Form4
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 231)
+        Me.Controls.Add(Me.txtpass)
+        Me.Controls.Add(Me.txtusuario)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form4"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form4"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txtusuario As System.Windows.Forms.TextBox
+    Friend WithEvents txtpass As System.Windows.Forms.TextBox
+End Class
---
+++ listacliente/Form4.vb
@@ -0,0 +1,76 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data.Sql
+Module Conexion
+
+    Public conexion As SqlConnection
+    Public enunciado As SqlCommand
+    Public respuesta As SqlDataReader
+    Sub abrir()
+        Try
+            conexion = New SqlConnection("Server=192.168.27.30;DATABASE=farmacia; User id= user; Password=pquinde")
+            conexion.Open()
+            MsgBox("Conectado", MsgBoxStyle.Information)
+        Catch ex As Exception
+            MsgBox("No se pudo conectar", MsgBoxStyle.Critical)
+        End Try
+    End Sub
+    Function usuarioRegistrado(ByVal nombreusuario As String) As Boolean
+        Dim resul As Boolean = False
+        Try
+            enunciado = New SqlCommand("Select * from usuarios Where usuario = '" & nombreusuario & "'", conexion)
+            respuesta = enunciado.ExecuteReader
+            If respuesta.Read Then
+                resul = respuesta.Item("contrasena")
+            End If
+            respuesta.Close()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+        Return resul
+    End Function
+    Function contrasena(ByVal nombreUsuario As String) As String
+        Dim resul As String = ""
+        Try
+
+        enunciado = New SqlCommand("Select contrasena from usuarios Where usuario = '" & nombreUsuario & "'", conexion)
+        respuesta = enunciado.ExecuteReader
+        If respuesta.Read Then
+            resul = respuesta.Item("contrasena")
+        End If
+            respuesta.Close()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+        Return resul
+    End Function
+End Module
+
+Public Class Form4
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            If usuarioRegistrado(txtusuario.Text) = True Then
+                Dim contra As String = contrasena(txtusuario.Text)
+                If contra.Equals(txtpass.Text) = True Then
+                    Form1.ShowDialog()
+                    txtusuario.Text = ""
+                    txtpass.Text = ""
+                Else
+                    MsgBox("Contraseña incorrecta", MsgBoxStyle.Critical)
+
+                    txtpass.Text = ""
+                End If
+            Else
+                MsgBox("El usuario: " + txtusuario.Text + " no se encuentra registrado", MsgBoxStyle.Critical)
+                txtusuario.Text = ""
+                txtpass.Text = ""
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Form4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        abrir()
+    End Sub
+End Class
